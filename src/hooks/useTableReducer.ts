f14bd9505295699f01c48fcd432b3327c8ac80cd
Tuwan <ping.zhang@xuncetech.com>
2023-02-22 17:09:08 +0800

    fix: 优化useTableReducer

--- src/hooks/useTableReducer.ts
+++ src/hooks/useTableReducer.ts
@@ -1,58 +1,109 @@
-// 前端自己筛选数据
-import { useReducer } from 'react';
+import { ReducerWithoutAction, useReducer } from 'react';
 
 interface IObject {
   [key: string]: any;
 }
+interface ITableReducer extends ReducerWithoutAction<any> {
+  data?: any[];
+  pageIndex?: string | number;
+  pageSize?: string | number;
+  isLoading?: boolean;
+  total?: number;
+  initParams?: IObject;
+}
 
 const tableReducer = (state: IObject, action: IObject) => {
   switch (action.type) {
-    // 进入页面首次拉取数据前的loading
-    case 'INIT':
+    // 进入页面首次拉取数据前的loading打开
+    case 'LOADING':
       return {
         ...state,
-        loading: true,
+        isLoading: true,
+      };
+    // 当前页码发生变化时的处理
+    case 'PAGE_INDEX':
+      return {
+        ...state,
+        isLoading: true,
+        pageIndex: action.payload,
+      };
+    // 每页条数发生变化时的处理
+    case 'PAGE_SIZE':
+      return {
+        ...state,
+        isLoading: true,
+        pageSize: action.payload,
       };
     // 接口请求数据成功时的处理
     case 'SUCCESS':
       return {
         ...state,
-        loading: false,
+        isLoading: false,
         ...action.payload,
       };
     // 接口请求数据失败时的处理
     case 'ERROR':
       return {
         ...state,
-        loading: false,
-        ...action.payload,
+        isLoading: false,
+        data: [],
+        total: 0,
       };
     // 新增删除假数据时的处理（此时只修改data其他均不发生变动）
     case 'CHANGE_DATA':
       return {
         ...state,
-        ...action.payload,
+        data: action.payload.data,
       };
     // 只关闭loading, 不做其他处理
     case 'CLOSE_LOADING':
       return {
         ...state,
-        loading: false,
+        isLoading: false,
       };
-    // 清空数据
-    case 'CLEAR':
+    // 修改列表接口参数
+    case 'CHANGE_PARAMS':
       return {
-        loading: false,
-        ...action.payload,
+        ...state,
+        params: action.payload?.params || {},
       };
     default:
-      return {};
+      return {
+        data: [],
+        pageIndex: 1,
+        pageSize: 10,
+        isLoading: false,
+        total: 0,
+        params: {},
+      };
   }
 };
 
-const useTableReducer = (initParams?: IObject) => {
-  const [stateObj, dispatch] = useReducer(tableReducer, { ...initParams });
-  return [stateObj, dispatch];
+const useTableReducer: ITableReducer = (
+  defaultPageSize = 10,
+  initParams = {},
+) => {
+  const [{ data, pageIndex, pageSize, isLoading, total, params }, dispatch] =
+    useReducer(tableReducer, {
+      data: [],
+      pageIndex: 1,
+      pageSize: defaultPageSize,
+      isLoading: false,
+      total: 0,
+      params: initParams || {},
+    });
+
+  return [
+    {
+      isLoading,
+      data,
+      total,
+      pageIndex,
+      pageSize,
+      params,
+    },
+    dispatch,
+  ];
 };
 
 export default useTableReducer;
--- src/pages/useTableReducerDemo/index.tsx
+++ src/pages/useTableReducerDemo/index.tsx
@@ -2,11 +2,12 @@ import { Button, Spin } from 'antd';
 import useTableReducer from '@/hooks/useTableReducer';
 
 const UseTableReducerDemo = () => {
-  const [{ data = [], loading = false }, dispatch] = useTableReducer();
+  const [{ isLoading, data, total, pageIndex, pageSize, params }, dispatch] =
+    useTableReducer(20);
 
-  /** 初始化 */
-  const initFn = () => {
-    dispatch({ type: 'INIT' });
+  /** 打开loading */
+  const loadingFn = () => {
+    dispatch({ type: 'LOADING' });
   };
   /** 成功 */
   const successFn = () => {
@@ -38,37 +39,107 @@ const UseTableReducerDemo = () => {
   const clearFn = () => {
     dispatch({ type: 'CLEAR' });
   };
+  /** 模拟接口 */
+  const ApiFn = (params: any) => {
+    console.log('参数:', params);
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        let res = {
+          pageIndex: params.pageIndex,
+          pageSize: params.pageSize,
+          records: [{ a: 1 }, { a: 2 }],
+          total: 100,
+        };
+        resolve(res);
+      }, 2000);
+    });
+  };
+  /** 查询 */
+  const searchFn = async (conditionParams: any) => {
+    try {
+      dispatch({ type: 'LOADING' }); // 打开loading
+      const res: any = await ApiFn({
+        pageIndex,
+        pageSize,
+        ...params,
+        ...conditionParams,
+      });
+      dispatch({
+        type: 'SUCCESS', // 会自动关闭loading
+        payload: {
+          data: res?.records,
+          total: res?.total,
+          pageIndex: res?.pageIndex,
+          pageSize: res?.pageSize,
+          params: { ...params, ...conditionParams },
+        },
+      });
+    } catch (err) {
+      dispatch({ type: 'ERROR' }); // 会自动关闭loading
+    } finally {
+      dispatch({ type: 'CLOSE_LOADING' });
+    }
+  };
+  /** 条件查询 */
+  const conditionSearchFn = () => {
+    searchFn({ c: 1, d: 2 });
+  };
+  /** 分页查询 */
+  const pageSearchFn = () => {
+    searchFn({ pageIndex: 2, pageSize: 3 });
+  };
 
   return (
     <div>
-      <Spin spinning={loading}>
+      <Spin spinning={isLoading}>
         <div style={{ height: '100px', border: '1px solid #000' }}>
-          表格数据：{JSON.stringify(data)}
+          表格数据：{JSON.stringify(data)} <br />
+          total: {total}
+          <br />
+          pageIndex: {pageIndex}
+          <br />
+          pageSize: {pageSize}
+          <br />
         </div>
       </Spin>
-      <Button type="primary" onClick={initFn}>
-        初始化
-      </Button>{' '}
+
+      <Button type="primary" onClick={loadingFn}>
+        打开loading
+      </Button>
       <br />
       <Button type="primary" onClick={successFn}>
         成功（存数据）
-      </Button>{' '}
+      </Button>
       <br />
       <Button type="primary" onClick={errorFn}>
         失败
-      </Button>{' '}
+      </Button>
       <br />
       <Button type="primary" onClick={changeDataFn}>
-        改变数据（单纯的改变）
-      </Button>{' '}
+        改变数据（单纯的改变data）
+      </Button>
       <br />
       <Button type="primary" onClick={closeLoadingFn}>
         关闭 loading
-      </Button>{' '}
+      </Button>
       <br />
       <Button type="primary" onClick={clearFn}>
         清空数据
-      </Button>{' '}
+      </Button>
+      <br />
+      <br />
+
+      <Button type="primary" onClick={searchFn}>
+        查询
+      </Button>
+      <br />
+      <Button type="primary" onClick={conditionSearchFn}>
+        条件查询
+      </Button>
+      <br />
+      <Button type="primary" onClick={pageSearchFn}>
+        分页查询
+      </Button>
       <br />
     </div>
   );
