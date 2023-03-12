08ed09cc1c8c2820eb6582762be62f6bbacfd459
zhangping <2425238116@qq.com>
2023-03-12 08:03:59 +0800

    fix: 优化换肤

--- src/pages/skinTheme/index.tsx
+++ src/pages/skinTheme/index.tsx
@@ -4,7 +4,8 @@ import styles from './index.module.less';
 const SkinTheme = () => {
   return (
     <div className={styles['skinTheme']}>
-      <Button type='primary'>按钮</Button>
+      <p>如果是单独启动子应用，则需要手动给body标签追加类名。如 'theme-gold'</p>
+      <Button type="primary">按钮</Button>
     </div>
   );
 };
