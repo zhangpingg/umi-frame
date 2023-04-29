581b5ad0fc16a0e6790eb0e5a8340e9b90be36ce
zhangping <2425238116@qq.com>
2023-04-29 22:43:41 +0800

    feat: spark-md5 计算文件内容的 hash

--- src/pages/sparkMD5Demo/index.tsx
+++ src/pages/sparkMD5Demo/index.tsx
@@ -7,7 +7,7 @@ const SparkMD5Demo = () => {
     const hash = sparkMD5.hashBinary('11'); // 6512bd43d9caa6e02c990b0a82652dca
     console.log('字符串hash:', hash);
   };
-  /** 上传文件后，计算文件内的hash */
+  /** 上传文件后，计算文件内容的hash */
   const changeFile = (e: any) => {
     const spark = new sparkMD5.ArrayBuffer(); // 创建md5实例（因为创建是ArrayBuffer，所以下面接收的也是这种格式）
     const reader = new FileReader();
