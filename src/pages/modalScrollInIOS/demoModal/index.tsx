ed2bd23bc44dfd039b5f8105e69d28b77d6720a3
zhangping <2425238116@qq.com>
2024-04-19 10:58:53 +0800

    feat: 清除IOS滚动问题抽离成hooks

--- src/pages/modalScrollInIOS/demoModal/index.tsx
+++ src/pages/modalScrollInIOS/demoModal/index.tsx
@@ -1,54 +1,19 @@
 import { useEffect, forwardRef, useState, useImperativeHandle } from 'react';
 import { Drawer } from 'antd';
+import { useClearIosScroll } from '@/hooks';
 import styles from './index.module.less';
 
 const Index = forwardRef((props, ref) => {
+  const { addTouchmove, removeTouchmove } = useClearIosScroll({
+    forbidList: ['.zp-ant-drawer-mask', '.dm-main-close'],
+    scrollName: '.dm-main-list',
+  });
   const [open, setOpen] = useState<boolean>(false);
 
-  // 阻止默认事件
-  const preventDefault = (e: any) => {
-    e.preventDefault();
-  };
-  // 设置滚动位置
-  const setScrollPosition = (dom: any) => {
-    const currentScrollTop = dom.scrollTop;
-    if (currentScrollTop < 1) {
-      dom.scrollTop = 1;
-    }
-    if (dom.clientHeight + dom.scrollTop >= dom.scrollHeight) {
-      dom.scrollTop = dom.scrollTop - 1;
-    }
-  };
-  // 添加touchmove
-  const addTouchmove = () => {
-    // 阻止遮盖层的默认事件
-    document
-      ?.querySelector('.zp-ant-drawer-mask')
-      ?.addEventListener('touchmove', preventDefault, { passive: false });
-    // 阻止关闭按钮的默认事件
-    document
-      ?.querySelector('.dm-main-close')
-      ?.addEventListener('touchmove', preventDefault, { passive: false });
-    const listDom: any = document.querySelector('.dm-main-list');
-    if (listDom) {
-      // 初次打开弹框的时候，初始化数据，滚动条置为1
-      listDom.style.overflow = 'scroll';
-      listDom.scrollTop = 1;
-      listDom.addEventListener('scroll', () => {
-        setScrollPosition(listDom);
-      });
-    }
-  };
   // 打开弹框
   const openCloseModal = (flag: boolean) => {
     if (!flag) {
-      document
-        ?.querySelector('.zp-ant-drawer-mask')
-        ?.removeEventListener('touchmove', preventDefault);
-      document
-        ?.querySelector('.dm-main-close')
-        ?.removeEventListener('touchmove', preventDefault);
-      window.removeEventListener('scroll', setScrollPosition);
+      removeTouchmove();
     }
     setOpen(flag);
   };
