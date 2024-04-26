42eb83855b8dd643ac18682ea5247db48c9e64f9
zhangping <2425238116@qq.com>
2024-04-26 17:15:20 +0800

    fix: 优化-createPortal的示例

--- src/pages/createPortalDemo/coverLayer/index.tsx
+++ src/pages/createPortalDemo/coverLayer/index.tsx
@@ -1,7 +1,8 @@
+import { createPortal } from 'react-dom';
 import styles from './index.module.less';
 
 const CoverLayer = () => {
-  return <div className={styles['box']}></div>;
+  return createPortal(<div className={styles['box']}></div>, document.body);
 };
 
 export default CoverLayer;
