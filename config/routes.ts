f8984e401abc59683f35fb220dbf58cabb21b1c5
zhangping <2425238116@qq.com>
2024-11-24 09:50:09 +0800

    feat: 省略号

--- config/routes.ts
+++ config/routes.ts
@@ -1,207 +1,212 @@
 export default [
-  {
-    path: '/',
-    component: '@/layouts',
-    routes: [
-      { path: '/', redirect: '/home' },
-      { path: '/home', component: '@/pages/home', exact: true },
-      { path: '/test', component: '@/pages/test', exact: true },
-      {
-        path: '/localResources',
-        title: '本地资源',
-        component: '@/pages/localResources',
-        exact: true,
-      },
-      {
-        path: '/useRedux',
-        component: '@/pages/useRedux',
-        exact: true,
-      },
-      {
-        path: '/useDva',
-        component: '@/pages/useDva',
-        exact: true,
-      },
-      {
-        path: '/useSetIntervalDemo',
-        component: '@/pages/useSetIntervalDemo',
-        exact: true,
-      },
-      {
-        path: '/useTableReducerDemo',
-        component: '@/pages/useTableReducerDemo',
-        exact: true,
-      },
-      {
-        path: '/useApi',
-        component: '@/pages/useApi',
-        exact: true,
-      },
-      {
-        path: '/cursorPosition',
-        component: '@/pages/cursorPosition',
-        exact: true,
-      },
-      {
-        path: '/watermark',
-        component: '@/pages/watermark',
-        exact: true,
-      },
-      {
-        path: '/responsiveGrid',
-        component: '@/pages/responsiveGrid',
-        exact: true,
-      },
-      {
-        path: '/tableRoll',
-        component: '@/pages/tableRoll',
-        exact: true,
-      },
-      {
-        path: '/antv',
-        component: '@/pages/antv',
-        exact: true,
-      },
-      {
-        path: '/antd',
-        component: '@/pages/antd',
-        exact: true,
-      },
-      {
-        path: '/skinTheme',
-        component: '@/pages/skinTheme',
-        exact: true,
-      },
-      {
-        path: '/performanceObserverDemo',
-        component: '@/pages/performanceObserverDemo',
-        exact: true,
-      },
-      {
-        path: '/frameStoreData',
-        component: '@/pages/frameStoreData',
-        exact: true,
-      },
-      {
-        path: '/memoDemo',
-        component: '@/pages/memoDemo',
-        exact: true,
-      },
-      {
-        path: '/cryptoJs',
-        component: '@/pages/cryptoJs',
-        exact: true,
-      },
-      {
-        path: '/mouseRightClick',
-        component: '@/pages/mouseRightClick',
-        exact: true,
-      },
-      {
-        path: '/createPortalDemo',
-        component: '@/pages/createPortalDemo',
-        exact: true,
-      },
-      {
-        path: '/workerDemo',
-        component: '@/pages/workerDemo',
-        exact: true,
-      },
-      {
-        path: '/FileReaderDemo',
-        component: '@/pages/FileReaderDemo',
-        exact: true,
-      },
-      {
-        path: '/uploadDemo',
-        component: '@/pages/uploadDemo',
-        exact: true,
-      },
-      {
-        path: '/uploadDemoTwo',
-        component: '@/pages/uploadDemoTwo',
-        exact: true,
-      },
-      {
-        path: '/sparkMD5Demo',
-        component: '@/pages/sparkMD5Demo',
-        exact: true,
-      },
-      {
-        path: '/reactActivationDemo',
-        component: '@/pages/reactActivationDemo',
-        exact: true,
-      },
-      {
-        path: '/longList',
-        component: '@/pages/longList',
-        exact: true,
-      },
-      {
-        path: '/useImperativeHandleDemo',
-        component: '@/pages/useImperativeHandleDemo',
-        exact: true,
-      },
-      {
-        path: '/useContextDemo',
-        component: '@/pages/useContextDemo',
-        exact: true,
-      },
-      {
-        path: '/dataDetailsDemo',
-        component: '@/pages/dataDetailsDemo',
-        exact: true,
-      },
-      {
-        path: '/domToImageDemo',
-        component: '@/pages/domToImageDemo',
-        exact: true,
-      },
-      {
-        path: '/lotteryDraw',
-        component: '@/pages/lotteryDraw',
-        exact: true,
-      },
-      {
-        path: '/animateDemo',
-        component: '@/pages/animateDemo',
-        exact: true,
-      },
-      {
-        path: '/smashEggH5',
-        component: '@/pages/smashEggH5',
-        exact: true,
-      },
-      {
-        path: '/modalScrollInIOSH5',
-        component: '@/pages/modalScrollInIOSH5',
-        exact: true,
-      },
-      {
-        path: '/scrollListH5',
-        component: '@/pages/scrollListH5',
-        exact: true,
-      },
-      {
-        path: '/compressImg',
-        component: '@/pages/compressImg',
-        exact: true,
-      },
-      {
-        path: '/svgaDemo',
-        component: '@/pages/svgaDemo',
-        exact: true,
-      },
-      {
-        path: '/stickyDemo',
-        component: '@/pages/stickyDemo',
-        exact: true,
-      },
-      {
-        path: '/curveAnimation',
-        component: '@/pages/curveAnimation',
-        exact: true,
-      },
-    ],
-  },
+    {
+        path: '/',
+        component: '@/layouts',
+        routes: [
+            { path: '/', redirect: '/home' },
+            { path: '/home', component: '@/pages/home', exact: true },
+            { path: '/test', component: '@/pages/test', exact: true },
+            {
+                path: '/localResources',
+                title: '本地资源',
+                component: '@/pages/localResources',
+                exact: true,
+            },
+            {
+                path: '/useRedux',
+                component: '@/pages/useRedux',
+                exact: true,
+            },
+            {
+                path: '/useDva',
+                component: '@/pages/useDva',
+                exact: true,
+            },
+            {
+                path: '/useSetIntervalDemo',
+                component: '@/pages/useSetIntervalDemo',
+                exact: true,
+            },
+            {
+                path: '/useTableReducerDemo',
+                component: '@/pages/useTableReducerDemo',
+                exact: true,
+            },
+            {
+                path: '/useApi',
+                component: '@/pages/useApi',
+                exact: true,
+            },
+            {
+                path: '/cursorPosition',
+                component: '@/pages/cursorPosition',
+                exact: true,
+            },
+            {
+                path: '/watermark',
+                component: '@/pages/watermark',
+                exact: true,
+            },
+            {
+                path: '/responsiveGrid',
+                component: '@/pages/responsiveGrid',
+                exact: true,
+            },
+            {
+                path: '/tableRoll',
+                component: '@/pages/tableRoll',
+                exact: true,
+            },
+            {
+                path: '/antv',
+                component: '@/pages/antv',
+                exact: true,
+            },
+            {
+                path: '/antd',
+                component: '@/pages/antd',
+                exact: true,
+            },
+            {
+                path: '/skinTheme',
+                component: '@/pages/skinTheme',
+                exact: true,
+            },
+            {
+                path: '/performanceObserverDemo',
+                component: '@/pages/performanceObserverDemo',
+                exact: true,
+            },
+            {
+                path: '/frameStoreData',
+                component: '@/pages/frameStoreData',
+                exact: true,
+            },
+            {
+                path: '/memoDemo',
+                component: '@/pages/memoDemo',
+                exact: true,
+            },
+            {
+                path: '/cryptoJs',
+                component: '@/pages/cryptoJs',
+                exact: true,
+            },
+            {
+                path: '/mouseRightClick',
+                component: '@/pages/mouseRightClick',
+                exact: true,
+            },
+            {
+                path: '/createPortalDemo',
+                component: '@/pages/createPortalDemo',
+                exact: true,
+            },
+            {
+                path: '/workerDemo',
+                component: '@/pages/workerDemo',
+                exact: true,
+            },
+            {
+                path: '/FileReaderDemo',
+                component: '@/pages/FileReaderDemo',
+                exact: true,
+            },
+            {
+                path: '/uploadDemo',
+                component: '@/pages/uploadDemo',
+                exact: true,
+            },
+            {
+                path: '/uploadDemoTwo',
+                component: '@/pages/uploadDemoTwo',
+                exact: true,
+            },
+            {
+                path: '/sparkMD5Demo',
+                component: '@/pages/sparkMD5Demo',
+                exact: true,
+            },
+            {
+                path: '/reactActivationDemo',
+                component: '@/pages/reactActivationDemo',
+                exact: true,
+            },
+            {
+                path: '/longList',
+                component: '@/pages/longList',
+                exact: true,
+            },
+            {
+                path: '/useImperativeHandleDemo',
+                component: '@/pages/useImperativeHandleDemo',
+                exact: true,
+            },
+            {
+                path: '/useContextDemo',
+                component: '@/pages/useContextDemo',
+                exact: true,
+            },
+            {
+                path: '/dataDetailsDemo',
+                component: '@/pages/dataDetailsDemo',
+                exact: true,
+            },
+            {
+                path: '/domToImageDemo',
+                component: '@/pages/domToImageDemo',
+                exact: true,
+            },
+            {
+                path: '/lotteryDraw',
+                component: '@/pages/lotteryDraw',
+                exact: true,
+            },
+            {
+                path: '/animateDemo',
+                component: '@/pages/animateDemo',
+                exact: true,
+            },
+            {
+                path: '/smashEggH5',
+                component: '@/pages/smashEggH5',
+                exact: true,
+            },
+            {
+                path: '/modalScrollInIOSH5',
+                component: '@/pages/modalScrollInIOSH5',
+                exact: true,
+            },
+            {
+                path: '/scrollListH5',
+                component: '@/pages/scrollListH5',
+                exact: true,
+            },
+            {
+                path: '/compressImg',
+                component: '@/pages/compressImg',
+                exact: true,
+            },
+            {
+                path: '/svgaDemo',
+                component: '@/pages/svgaDemo',
+                exact: true,
+            },
+            {
+                path: '/stickyDemo',
+                component: '@/pages/stickyDemo',
+                exact: true,
+            },
+            {
+                path: '/curveAnimation',
+                component: '@/pages/curveAnimation',
+                exact: true,
+            },
+            {
+                path: '/ellipsisDemo',
+                component: '@/pages/ellipsisDemo',
+                exact: true,
+            },
+        ],
+    },
 ];
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,173 +3,184 @@ import { Button } from 'antd';
 import { useCallback } from 'react';
 
 const Home = () => {
-  const jumpPage = useCallback((path) => {
-    history.push(`/${path}`);
-  }, []);
+    const jumpPage = useCallback((path) => {
+        history.push(`/${path}`);
+    }, []);
 
-  return (
-    <div className={'global-box'}>
-      <Button type="link" onClick={() => jumpPage('test')}>
-        test
-      </Button>
-      <br />
-      <Button type="link" onClick={() => jumpPage('localResources')}>
-        本地静态资源
-      </Button>{' '}
-      <br />
-      <Button type="link" onClick={() => jumpPage('useRedux')}>
-        @reduxjs/toolkit
-      </Button>
-      <br />
-      <Button type="link" onClick={() => jumpPage('useDva')}>
-        dva
-      </Button>
-      <br />
-      <Button type="link" onClick={() => jumpPage('useTableReducerDemo')}>
-        useTableReducer demo使用
-      </Button>
-      <br />
-      <Button type="link" onClick={() => jumpPage('useSetIntervalDemo')}>
-        useSetInterval demo使用
-      </Button>
-      <br />
-      <Button type="link" onClick={() => jumpPage('useApi')}>
-        调接口
-      </Button>
-      <br />
-      <Button type="link" onClick={() => jumpPage('cursorPosition')}>
-        获取光标、设置光标、插入数据
-      </Button>
-      <br />
-      <Button type="link" onClick={() => jumpPage('watermark')}>
-        水印
-      </Button>
-      <br />
-      <Button type="link" onClick={() => jumpPage('responsiveGrid')}>
-        react-grid-layout 响应布局
-      </Button>
-      <br />
-      <Button type="link" onClick={() => jumpPage('tableRoll')}>
-        table 内容自动滚动
-      </Button>
-      <br />
-      <Button type="link" onClick={() => jumpPage('antv')}>
-        antv 图表
-      </Button>
-      <br />
-      <Button type="link" onClick={() => jumpPage('antd')}>
-        antd
-      </Button>
-      <br />
-      <Button type="link" onClick={() => jumpPage('skinTheme')}>
-        换肤
-      </Button>
-      <br />
-      <Button type="link" onClick={() => jumpPage('performanceObserverDemo')}>
-        PerformanceObserver 监听请求资源
-      </Button>
-      <br />
-      <Button type="link" onClick={() => jumpPage('frameStoreData')}>
-        获取基座的缓存数
-      </Button>
-      <br />
-      <Button type="link" onClick={() => jumpPage('memoDemo')}>
-        memo 例子
-      </Button>
-      <br />
-      <Button type="link" onClick={() => jumpPage('cryptojs')}>
-        crypto-ts 加密解密
-      </Button>
-      <br />
-      <Button type="link" onClick={() => jumpPage('mouseRightClick')}>
-        自定义鼠标右击
-      </Button>
-      <br />
-      <Button type="link" onClick={() => jumpPage('createPortalDemo')}>
-        创建节点在 DOM 组件的层次结构之外, 如modal,message等
-      </Button>
-      <br />
-      <Button type="link" onClick={() => jumpPage('workerDemo')}>
-        Worker 多线程
-      </Button>
-      <br />
-      <Button type="link" onClick={() => jumpPage('FileReaderDemo')}>
-        FileReader异步读取文件 (读取blob对象中的数据)
-      </Button>
-      <br />
-      <Button type="link" onClick={() => jumpPage('uploadDemo')}>
-        Upload 各种上传方式
-      </Button>
-      <br />
-      <Button type="link" onClick={() => jumpPage('uploadDemoTwo')}>
-        Upload 各种上传方式two，预览图片（更简单）
-      </Button>
-      <br />
-      <Button type="link" onClick={() => jumpPage('sparkMD5Demo')}>
-        spark-md5 计算文件内容的 hash
-      </Button>
-      <br />
-      <Button type="link" onClick={() => jumpPage('reactActivationDemo')}>
-        react-activation(依赖) 缓存组件，保留组件状态
-      </Button>
-      <br />
-      <Button type="link" onClick={() => jumpPage('longList')}>
-        大数据-长列表【时间分片 | 虚拟列表（列表项固定高度|列表项随机高度）】
-      </Button>
-      <br />
-      <Button type="link" onClick={() => jumpPage('useImperativeHandleDemo')}>
-        父组件调用子组件方法-useImperativeHandle
-      </Button>
-      <br />
-      <Button type="link" onClick={() => jumpPage('useContextDemo')}>
-        父子传值-useContext
-      </Button>
-      <br />
-      <Button type="link" onClick={() => jumpPage('dataDetailsDemo')}>
-        详情组件 (后期迁移到zp-design组件库中)
-      </Button>
-      <br />
-      <Button type="link" onClick={() => jumpPage('domToImageDemo')}>
-        Dom转换为图片
-      </Button>
-      <br />
-      <Button type="link" onClick={() => jumpPage('lotteryDraw')}>
-        转盘/九宫格/老虎机-抽奖
-      </Button>
-      <br />
-      <Button type="link" onClick={() => jumpPage('animateDemo')}>
-        Animate.css动画
-      </Button>
-      <br />
-      <Button type="link" onClick={() => jumpPage('smashEggH5')}>
-        砸金蛋抽奖【H5移动端】
-      </Button>
-      <br />
-      <Button type="link" onClick={() => jumpPage('modalScrollInIOSH5')}>
-        IOS：弹框中滚动内容，影响了父页面滚动【H5移动端】
-      </Button>
-      <br />
-      <Button type="link" onClick={() => jumpPage('scrollListH5')}>
-        无线滚动【H5移动端】
-      </Button>
-      <br />
-      <Button type="link" onClick={() => jumpPage('compressImg')}>
-        压缩图片
-      </Button>
-      <br />
-      <Button type="link" onClick={() => jumpPage('svgaDemo')}>
-        播放SVGA格式动画
-      </Button>
-      <br />
-      <Button type="link" onClick={() => jumpPage('stickyDemo')}>
-        粘性布局-如粘性导航栏，侧边栏等
-      </Button>
-      <br />
-      <Button type="link" onClick={() => jumpPage('curveAnimation')}>
-        两固定点之间-实现曲线运动
-      </Button>
-      <br />
-    </div>
-  );
+    return (
+        <div className={'global-box'}>
+            <Button type="link" onClick={() => jumpPage('test')}>
+                test
+            </Button>
+            <br />
+            <Button type="link" onClick={() => jumpPage('localResources')}>
+                本地静态资源
+            </Button>{' '}
+            <br />
+            <Button type="link" onClick={() => jumpPage('useRedux')}>
+                @reduxjs/toolkit
+            </Button>
+            <br />
+            <Button type="link" onClick={() => jumpPage('useDva')}>
+                dva
+            </Button>
+            <br />
+            <Button type="link" onClick={() => jumpPage('useTableReducerDemo')}>
+                useTableReducer demo使用
+            </Button>
+            <br />
+            <Button type="link" onClick={() => jumpPage('useSetIntervalDemo')}>
+                useSetInterval demo使用
+            </Button>
+            <br />
+            <Button type="link" onClick={() => jumpPage('useApi')}>
+                调接口
+            </Button>
+            <br />
+            <Button type="link" onClick={() => jumpPage('cursorPosition')}>
+                获取光标、设置光标、插入数据
+            </Button>
+            <br />
+            <Button type="link" onClick={() => jumpPage('watermark')}>
+                水印
+            </Button>
+            <br />
+            <Button type="link" onClick={() => jumpPage('responsiveGrid')}>
+                react-grid-layout 响应布局
+            </Button>
+            <br />
+            <Button type="link" onClick={() => jumpPage('tableRoll')}>
+                table 内容自动滚动
+            </Button>
+            <br />
+            <Button type="link" onClick={() => jumpPage('antv')}>
+                antv 图表
+            </Button>
+            <br />
+            <Button type="link" onClick={() => jumpPage('antd')}>
+                antd
+            </Button>
+            <br />
+            <Button type="link" onClick={() => jumpPage('skinTheme')}>
+                换肤
+            </Button>
+            <br />
+            <Button
+                type="link"
+                onClick={() => jumpPage('performanceObserverDemo')}
+            >
+                PerformanceObserver 监听请求资源
+            </Button>
+            <br />
+            <Button type="link" onClick={() => jumpPage('frameStoreData')}>
+                获取基座的缓存数
+            </Button>
+            <br />
+            <Button type="link" onClick={() => jumpPage('memoDemo')}>
+                memo 例子
+            </Button>
+            <br />
+            <Button type="link" onClick={() => jumpPage('cryptojs')}>
+                crypto-ts 加密解密
+            </Button>
+            <br />
+            <Button type="link" onClick={() => jumpPage('mouseRightClick')}>
+                自定义鼠标右击
+            </Button>
+            <br />
+            <Button type="link" onClick={() => jumpPage('createPortalDemo')}>
+                创建节点在 DOM 组件的层次结构之外, 如modal,message等
+            </Button>
+            <br />
+            <Button type="link" onClick={() => jumpPage('workerDemo')}>
+                Worker 多线程
+            </Button>
+            <br />
+            <Button type="link" onClick={() => jumpPage('FileReaderDemo')}>
+                FileReader异步读取文件 (读取blob对象中的数据)
+            </Button>
+            <br />
+            <Button type="link" onClick={() => jumpPage('uploadDemo')}>
+                Upload 各种上传方式
+            </Button>
+            <br />
+            <Button type="link" onClick={() => jumpPage('uploadDemoTwo')}>
+                Upload 各种上传方式two，预览图片（更简单）
+            </Button>
+            <br />
+            <Button type="link" onClick={() => jumpPage('sparkMD5Demo')}>
+                spark-md5 计算文件内容的 hash
+            </Button>
+            <br />
+            <Button type="link" onClick={() => jumpPage('reactActivationDemo')}>
+                react-activation(依赖) 缓存组件，保留组件状态
+            </Button>
+            <br />
+            <Button type="link" onClick={() => jumpPage('longList')}>
+                大数据-长列表【时间分片 |
+                虚拟列表（列表项固定高度|列表项随机高度）】
+            </Button>
+            <br />
+            <Button
+                type="link"
+                onClick={() => jumpPage('useImperativeHandleDemo')}
+            >
+                父组件调用子组件方法-useImperativeHandle
+            </Button>
+            <br />
+            <Button type="link" onClick={() => jumpPage('useContextDemo')}>
+                父子传值-useContext
+            </Button>
+            <br />
+            <Button type="link" onClick={() => jumpPage('dataDetailsDemo')}>
+                详情组件 (后期迁移到zp-design组件库中)
+            </Button>
+            <br />
+            <Button type="link" onClick={() => jumpPage('domToImageDemo')}>
+                Dom转换为图片
+            </Button>
+            <br />
+            <Button type="link" onClick={() => jumpPage('lotteryDraw')}>
+                转盘/九宫格/老虎机-抽奖
+            </Button>
+            <br />
+            <Button type="link" onClick={() => jumpPage('animateDemo')}>
+                Animate.css动画
+            </Button>
+            <br />
+            <Button type="link" onClick={() => jumpPage('smashEggH5')}>
+                砸金蛋抽奖【H5移动端】
+            </Button>
+            <br />
+            <Button type="link" onClick={() => jumpPage('modalScrollInIOSH5')}>
+                IOS：弹框中滚动内容，影响了父页面滚动【H5移动端】
+            </Button>
+            <br />
+            <Button type="link" onClick={() => jumpPage('scrollListH5')}>
+                无线滚动【H5移动端】
+            </Button>
+            <br />
+            <Button type="link" onClick={() => jumpPage('compressImg')}>
+                压缩图片
+            </Button>
+            <br />
+            <Button type="link" onClick={() => jumpPage('svgaDemo')}>
+                播放SVGA格式动画
+            </Button>
+            <br />
+            <Button type="link" onClick={() => jumpPage('stickyDemo')}>
+                粘性布局-如粘性导航栏，侧边栏等
+            </Button>
+            <br />
+            <Button type="link" onClick={() => jumpPage('curveAnimation')}>
+                两固定点之间-实现曲线运动
+            </Button>
+            <br />
+            <Button type="link" onClick={() => jumpPage('ellipsisDemo')}>
+                省略号（单行、多行、更多等）
+            </Button>
+            <br />
+        </div>
+    );
 };
 export default Home;
